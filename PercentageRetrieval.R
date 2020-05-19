c0fbafc18bfe9119826d28b922cd02c1c402ee91
adriments <adriments@gmail.com>
2020-05-19 13:47:42 -0400

    Changed Variable Names

--- PercentageRetrieval.R
+++ PercentageRetrieval.R
@@ -8,8 +8,8 @@ HouseData <- data.frame("Provinces" = sample(1:12, 10000, replace=TRUE),
 #First we'll explore creating percentages using table, prop.table, and converting it.
 
 HouseDataTable <- table(HouseData)
-HDTablePercents.1 <- prop.table(HouseDataTable, margin = 1)
-HDTablePercents.2 <- prop.table(HouseDataTable, margin = 2)
+PropTablePercents.1 <- prop.table(HouseDataTable, margin = 1)
+PropTablePercents.2 <- prop.table(HouseDataTable, margin = 2)
 #margin default is null, assumes entire table sums to 1 
 #margin = 1 sums across rows to equal 1, so finding out how all energy friendliness level is spread across a province.
 #margin = 2 sums down columns to equal 1, which will find out how a energy friendliness level is spread across all provinces.
@@ -17,7 +17,7 @@ HDTablePercents.2 <- prop.table(HouseDataTable, margin = 2)
 #you can convert the table into percentages with an apply() function 
 #see:https://stackoverflow.com/questions/9185745/appending-sign-in-output-of-prop-table
 
-HDTableView.1 <- apply(HDTablePercents.1*100, 2, function(u) sprintf( "%.1f%%", u))
-HDTableView.2 <- apply(HDTablePercents.2*100, 2, function(u) sprintf( "%.1f%%", u))
+PropTableView.1 <- apply(HDTablePercents.1*100, 2, function(u) sprintf( "%.1f%%", u))
+PropTableView.2 <- apply(HDTablePercents.2*100, 2, function(u) sprintf( "%.1f%%", u))
 
 #The above put simply multiplies every entry by 100
